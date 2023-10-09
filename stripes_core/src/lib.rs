a3c45a4bf9ee931f8c2d6f65823a1e986797fcb5
Lukas412 <lukas.weber.4.12@gmail.com>
2023-10-09 11:39:03 +0200

    add structs to lib

--- stripes_core/src/lib.rs
+++ stripes_core/src/lib.rs
@@ -1,1 +1,18 @@
+pub struct Animation {
+    statt: Frame,
+    changes: [Changes],
+}
 
+struct Frame {
+    parts: Vec<Part>,
+}
+
+enum Part {}
+
+struct Changes {
+    changes: Vec<Change>,
+}
+
+enum Change {
+    Part(Part),
+}
