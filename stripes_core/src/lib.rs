9364026709ba959a79388801a7c9d65f9a3346db
Lukas412 <lukas.weber.4.12@gmail.com>
2023-10-13 11:06:38 +0200

    move part to module

--- stripes_core/src/lib.rs
+++ stripes_core/src/lib.rs
@@ -2,10 +2,12 @@ use std::fmt::{Display, Formatter};
 
 mod changes;
 mod frame;
+mod part;
 
 pub use {
     changes::{Change, Changes},
     frame::Frame,
+    part::{LinearGradient, Part, SolidColor},
 };
 
 pub struct Animation {
@@ -38,69 +40,6 @@ impl Display for Animation {
             write!(f, "{}", changes)?;
         }
         Ok(())
-    }
-}
-
-pub enum Part {
-    Solid(SolidColor),
-    Linear(LinearGradient),
-}
-
-impl Display for Part {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Part::Solid(color) => {
-                write!(f, "{}", color)
-            }
-            Part::Linear(gradient) => {
-                write!(f, "{}", gradient)
-            }
-        }
-    }
-}
-
-pub struct SolidColor {
-    color: Color,
-    length: u8,
-}
-
-impl SolidColor {
-    pub fn new(color: Color, mut length: u8) -> Self {
-        if length == 0 {
-            length = 1;
-        }
-        Self { color, length }
-    }
-
-    pub fn single(color: Color) -> Self {
-        Self { color, length: 1 }
-    }
-}
-
-impl Display for SolidColor {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "c{}l{}", self.color, self.length)
-    }
-}
-
-pub struct LinearGradient {
-    start: Color,
-    end: Color,
-    length: u8,
-}
-
-impl LinearGradient {
-    pub fn new(start: Color, end: Color, mut length: u8) -> Self {
-        if length <= 1 {
-            length = 2;
-        }
-        Self { start, end, length }
-    }
-}
-
-impl Display for LinearGradient {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "s{}e{}l{}", self.start, self.end, self.length)
     }
 }
 
---
+++ stripes_core/src/part.rs
@@ -0,0 +1,65 @@
+use crate::Color;
+use std::fmt::{Display, Formatter};
+
+pub enum Part {
+    Solid(SolidColor),
+    Linear(LinearGradient),
+}
+
+impl Display for Part {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Part::Solid(color) => {
+                write!(f, "{}", color)
+            }
+            Part::Linear(gradient) => {
+                write!(f, "{}", gradient)
+            }
+        }
+    }
+}
+
+pub struct SolidColor {
+    color: Color,
+    length: u8,
+}
+
+impl SolidColor {
+    pub fn new(color: Color, mut length: u8) -> Self {
+        if length == 0 {
+            length = 1;
+        }
+        Self { color, length }
+    }
+
+    pub fn single(color: Color) -> Self {
+        Self { color, length: 1 }
+    }
+}
+
+impl Display for SolidColor {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "c{}l{}", self.color, self.length)
+    }
+}
+
+pub struct LinearGradient {
+    start: Color,
+    end: Color,
+    length: u8,
+}
+
+impl LinearGradient {
+    pub fn new(start: Color, end: Color, mut length: u8) -> Self {
+        if length <= 1 {
+            length = 2;
+        }
+        Self { start, end, length }
+    }
+}
+
+impl Display for LinearGradient {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "s{}e{}l{}", self.start, self.end, self.length)
+    }
+}
